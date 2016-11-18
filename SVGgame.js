ee64ba74ec16f21f1981e74d61fddb300e9bd186
Carlos Casas <albertocasas05@gmail.com>
2016-11-18 11:01:44 -0800

    tried

--- SVGgame.js
+++ SVGgame.js
@@ -2,15 +2,16 @@
   var yveltalposY = 300;
 
   document.addEventListener("keydown", function(e) {
-  if(e.keyCode == 37) {
+  if(e.keyCode == 37 ) {
     yveltalposX -= 10;
+
+    document.getElementById("yveltal").setAttribute("x", yveltalposX);
+  }
+  else if(e.keyCode == 39) {
+    yveltalposY += 10;
     document.getElementById("yveltal").setAttribute("x", yveltalposX);
   }
 
-  else if(e.keyCode == 39) {
-    yveltalposX += 10;
-    document.getElementById("yveltal").setAttribute("x", yveltalposX);
-  }
   })
 
   document.addEventListener("keydown", function(e) {
@@ -25,15 +26,15 @@
   }
   })
 // Overlap
-  var canvas = document.getElementById("svgGame");
-  var width = 30;
-  var height = 30;
-  var playerX
-  var playerY
-  var foodX
-  var foodY
+  var canvas1 = document.getElementById("playerArea");
+  var width = 85;
+  var height = 85;
+  var playerX;
+  var playerY;
+  var foodX;
+  var foodY;
 
-  canvas.addEventListener("keydown", function(e) {
+  canvas1.addEventListener("keydown", function(e) {
     var yveltal = document.getElementById("yveltal");
     var rarecandy = document.getElementById("rcandy");
 
@@ -43,36 +44,12 @@
     foodX = Number(rarecandy.getAttribute("x"));
     foodY = Number(rarecandy.getAttribute("y"));
 
-    if(playerX > foodX){
-      yveltal.setAttribute("opacity", 1);
+    if(playerX > foodX && playerX < foodX + width && playerY > foodY && playerY < foodY + height) {
       rarecandy.setAttribute("opacity", 0);
+      console.log("eaten");
     }
     else {
-      yveltal.setAttribute("opacity", 1);
       rarecandy.setAttribute("opacity", 1);
     }
-    if(playerX < foodX + width) {
-      yveltal.setAttribute("opacity", 1);
-      rarecandy.setAttribute("opacity", 0);
-    }
-    else {
-      yveltal.setAttribute("opacity", 1);
-      rarecandy.setAttribute("opacity", 1);
-    }
-    if(playerY > foodY) {
-      yveltal.setAttribute("opacity", 1);
-      rarecandy.setAttribute("opacity", 0);
-    }
-    else {
-      yveltal.setAttribute("opacity", 1);
-      rarecandy.setAttribute("opacity", 1);
-    }
-    if(playerY < foodY + height) {
-      yveltal.setAttribute("opacity", 1);
-      rarecandy.setAttribute("opacity", 0);
-    }
-    else {
-      yveltal.setAttribute("opacity", 1);
-      rarecandy.setAttribute("opacity", 1);
-    }
+
   })
