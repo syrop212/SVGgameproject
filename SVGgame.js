bf064eac85917f3d709c59fe8c7823e5151f61de
Carlos Casas <albertocasas05@gmail.com>
2016-11-28 10:35:30 -0800

    works finally

--- SVGgame.js
+++ SVGgame.js
@@ -1,30 +1,3 @@
-  var yveltalposX = 700;
-  var yveltalposY = 300;
-
-  document.addEventListener("keydown", function(e) {
-  if(e.keyCode == 37 ) {
-    yveltalposX -= 10;
-
-    document.getElementById("yveltal").setAttribute("x", yveltalposX);
-  }
-  else if(e.keyCode == 39) {
-    yveltalposY += 10;
-    document.getElementById("yveltal").setAttribute("x", yveltalposX);
-  }
-
-  })
-
-  document.addEventListener("keydown", function(e) {
-  if(e.keyCode == 38) {
-    yveltalposY -= 10;
-    document.getElementById("yveltal").setAttribute("y", yveltalposY);
-  }
-
-  else if(e.keyCode == 40) {
-    yveltalposY += 10;
-    document.getElementById("yveltal").setAttribute("y", yveltalposY);
-  }
-  })
 // Overlap
   var canvas1 = document.getElementById("playerArea");
   var width = 85;
@@ -33,23 +6,46 @@
   var playerY;
   var foodX;
   var foodY;
+  // player 
+  var yveltalposX = 700;
+  var yveltalposY = 300;
 
-  canvas1.addEventListener("keydown", function(e) {
-    var yveltal = document.getElementById("yveltal");
-    var rarecandy = document.getElementById("rcandy");
 
-    playerX = Number(yveltal.getAttribute("x"));
-    playerY = Number(yveltal.getAttribute("y"));
+document.addEventListener("keydown", function(e) {
+if(e.keyCode == 37 || e.keyCode == 65){
+  yveltalposX -= 10
+ document.getElementById("yveltal").setAttribute("x", yveltalposX)
+  }
+  else if(e.keyCode == 39|| e.keyCode == 68){
+    yveltalposX +=10;
+ document.getElementById("yveltal").setAttribute("x", yveltalposX)
+  }
+  if(e.keyCode == 40 || e.keyCode == 83){
+    yveltalposY +=10;
+ document.getElementById("yveltal").setAttribute("y", yveltalposY)
+  }
+  else if(e.keyCode == 38|| e.keyCode == 87){
+   yveltalposY -=10;
+ document.getElementById("yveltal").setAttribute("y", yveltalposY)
+}
 
-    foodX = Number(rarecandy.getAttribute("x"));
-    foodY = Number(rarecandy.getAttribute("y"));
+var yveltal = document.getElementById("yveltal");
+var rarecandy = document.getElementById("rcandy");
 
-    if(playerX > foodX && playerX < foodX + width && playerY > foodY && playerY < foodY + height) {
-      rarecandy.setAttribute("opacity", 0);
-      console.log("eaten");
-    }
-    else {
-      rarecandy.setAttribute("opacity", 1);
-    }
+
+playerX = Number(yveltal.getAttribute("x"));
+playerY = Number(yveltal.getAttribute("y"));
+
+foodX = Number(rarecandy.getAttribute("x"));
+foodY = Number(rarecandy.getAttribute("y"));
+
+if(playerX > foodX && playerX < foodX + width && playerY > foodY && playerY < foodY + height) {
+  rarecandy.setAttribute("opacity", 0);
+
+}
+else {
+  rarecandy.setAttribute("opacity", 1);
+}
+
 
   })
